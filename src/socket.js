df2a45ca609451c133805746dbb7be89cee34f8d
myth7siu <arunodaybagani969@gmail.com>
2026-03-15 03:19:01 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.DEV ? '' : window.location.origin;
+const URL = import.meta.env.VITE_SOCKET_URL;
 
 const socket = io(URL, {
   autoConnect: true,
